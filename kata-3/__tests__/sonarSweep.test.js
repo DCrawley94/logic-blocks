dd3fc382eb8d314ad854fa4f085da546a218568a
DCrawley94 <duncan.crawley@gmail.com>
2022-05-11 21:28:09 +0100

    streamline tests

--- kata-3/__tests__/sonarSweep.test.js
+++ kata-3/__tests__/sonarSweep.test.js
@@ -1,16 +1,11 @@
 const sonarSweep = require('../sonarSweep');
 
 describe('sonarSweep()', () => {
-	test('should count the amount of depth increases when each depth is larger than the last', () => {
+	test('should correctly count increases in depth', () => {
 		expect(sonarSweep([1, 2, 3])).toBe(2);
 	});
-	test('should count the amount of depth increases when each depth is smaller than the last', () => {
+	test("should ignore anything that isn't an increase in depth", () => {
 		expect(sonarSweep([3, 2, 1])).toBe(0);
-	});
-	test('should only increase the count when values are greater than the previous - skip if values are the same', () => {
 		expect(sonarSweep([3, 3, 3])).toBe(0);
-	});
-	test('should correctly calculate increases when depths are a mixture of increases and decreases', () => {
-		expect(sonarSweep([1, 2, 3, 3, 2, 1])).toBe(2);
 	});
 });
